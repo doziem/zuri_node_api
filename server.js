86915c64bf9461b954542310b7c4a40884e1ba10
Kenneth Chidozie <agbokennethchidoziem@gmail.com>
2024-05-13 14:45:32 -0700

    done with the implemetation

--- server.js
+++ server.js
@@ -1,73 +1,144 @@
-const {createServer} = require('http');
+
+const { createServer } = require("http");
+const url = require('url');
 
 const PORT = 3000;
 
-      const handleApiRequest =(req, res) => {
+let parsedUrl;
+const handleApiRequest = (req, res) => {
+   parsedUrl = url.parse(req.url, true);
 
-        if (req.url === "/" && req.method === "POST") {
+  switch (req.method) {
+    case "PATCH":
+      if (parsedUrl.pathname.startsWith("/joke/")) updateJoke(req,res) 
+      break;
+    case "GET":
+      if (req.url === "/") getAllJokes(req, res);
+      break;
+    case "POST":
+      if (req.url === "/") createJoke(req, res);
+      break;
 
-          createJoke(req, res);
+    case "DELETE":
+      if (parsedUrl.pathname.startsWith("/joke/")) deleteJoke(req,res);
+      break;
 
-        }else if(req.url === "/" && req.method === "GET"){
+    default:
+      res.writeHead(404);
+      res.end(JSON.stringify({ error: true, message: "Joke not Found" }));
+  }
+};
 
-          getAllJokes(req, res);
+const server = createServer(handleApiRequest);
 
-        }else if(req.url === "/joke/:id" && req.method === "PATCH"){
+// Generate ID
+const generateId = () => {
+  return +(Math.random() * 100000000).toFixed(0);
+};
 
-          updateJoke(req, res);
-
-        }else if(req.url === "/joke/:id" && req.method === "DELETE"){
-
-          deleteJoke(req, res);
-
-        }else{
-
-        }
-      }
-
-  const server = createServer(handleApiRequest);
-
-  // Generate ID
-  const generateId = () => {
-    return +( Math.random()*100000000).toFixed(0);
-  };
-
-     
-      // database
-      let db = [
-        {
-          id: generateId(),
-          title: "Why was the mushroom always invited to parties?",
-          comedian: "Bovi",
-          year: 2000,
-        },
-      ];
+// database
+let db = [
+  {
+    id: 41266273,
+    title: "Why was the mushroom always invited to parties?",
+    comedian: "Bovi",
+    year: 2000,
+  },
+];
 
 // Fetch All the Jokes
-const getAllJokes=(req,res)=>{
-
+const getAllJokes = (_, res) => {
   res.writeHead(200);
 
-  res.end(JSON.stringify({data:db, message:"Jokes Fetched Successfully"}))
-}
+  res.end(JSON.stringify({ data: db, message: "Jokes Fetched Successfully" }));
+};
 
 // Create Joke
-  function createJoke (req, res){
-    
-   
-    res.end(JSON.stringify(req.body));
-  };
+const createJoke = (req, res) => {
+
+  res.writeHead(201);
+
+  const body = [];
+  req.on("data", (chunk) => {
+    body.push(chunk);
+  });
+
+
+  req.on("end", () => {
+    const bodyBuffer = Buffer.concat(body).toString();
+
+    const jsonBody = JSON.parse(bodyBuffer);
+    const newnJoke = { ...jsonBody, id: generateId() };
+    db.push(newnJoke);
+
+    res.end(JSON.stringify({data:newnJoke,message:"Joke Successfully Created"}))
+    console.log(db);
+  });
+};
 
 // Update Joke
-  const updateJoke=(req,res)=>{
+const updateJoke = (req, res) => {
+  res.writeHead(200);
+  const jokeId = +parsedUrl.pathname.split("/")[2];
 
-  }
+  const body = [];
+  req.on("data", (chunk) => {
+    body.push(chunk);
+  });
+
+    req.on("end", () => {
+
+      const bodyBuffer = Buffer.concat(body).toString();
+
+    const jsonBody = JSON.parse(bodyBuffer);
+
+    const updatedDb = db.map(joke=>{
+      if(+joke.id===jokeId){
+        return {
+          ...joke,
+          ...jsonBody
+        }
+      }else{
+        return joke
+      }
+    })
+
+    db = updatedDb;
+
+      res.end(
+        JSON.stringify({ data: db, message: "Jokes Updated Successfully" })
+      );
+    });
+  
+
+};
 
 // Delete Joke
-  const deleteJoke=(req,res)=>{
+const deleteJoke = (req, res) => {
+  res.writeHead(200);
 
-  }
-  
-  server.listen(PORT, () => {
-    console.log(`Server is running on Port:${PORT}`);
+  const jokeId = +parsedUrl.pathname.split("/")[2];
+
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk;
   });
+
+  req.on("end", () => {
+    const newJokes = db.find((item) => +item.id === jokeId);
+
+    const removeJoked = db.splice(newJokes, 1)[0];
+
+    console.log(db);
+    res.end(
+      JSON.stringify({
+        data: removeJoked,
+        message: "Jokes Deleted Successfully",
+      })
+    );
+  });
+};
+
+server.listen(PORT, () => {
+  console.log(`Server is running on Port:${PORT}`);
+});
